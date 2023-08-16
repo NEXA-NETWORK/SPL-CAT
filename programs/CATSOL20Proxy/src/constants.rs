d64edeb0d7960fdbafebbbead5b8e8f0e1113cd9
Hamza Khalid <hamzakhalid829@gmail.com>
2023-08-16 11:15:01 +0500

    Chore: updated constant name

--- programs/CATSOL20Proxy/src/constants.rs
+++ programs/CATSOL20Proxy/src/constants.rs
@@ -4,4 +4,4 @@ pub const CORE_BRIDGE_ADDRESS: &str = "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY
 pub const SEED_PREFIX_SENT: &[u8; 4] = b"sent";
 
 // SEED Prefix for the Mint Account
-pub const SEED_PREFIX_MINT: &'static [u8; 13] = b"cat_sol_proxy";
+pub const SEED_PREFIX_LOCK: &'static [u8; 13] = b"cat_sol_proxy";
