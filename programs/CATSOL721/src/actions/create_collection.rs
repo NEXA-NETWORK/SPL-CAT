4ea1b6b5daef08913513f09e21633eb0cb8228f6
Hamza Khalid <hamzakhalid829@gmail.com>
2023-08-28 15:39:52 +0500

    Feat: Create NFT Collection

--- programs/CATSOL721/src/actions/create_collection.rs
+++ programs/CATSOL721/src/actions/create_collection.rs
@@ -1,32 +1,32 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
-    token::{Mint, MintTo, Token, TokenAccount, mint_to},
-    metadata::Metadata,
+    associated_token::AssociatedToken,
+    metadata::{
+        create_master_edition_v3, create_metadata_accounts_v3,
+        sign_metadata, CreateMasterEditionV3,
+        CreateMetadataAccountsV3, Metadata, SignMetadata,
+    },
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
+
+use mpl_token_metadata::{
+    pda::{find_master_edition_account, find_metadata_account},
+    state::{CollectionDetails, Creator, DataV2},
 };
 
 use crate::{
     constants::*,
     error::ErrorFactory,
-    state::{Config}
+    state::Config
 };
 
-use anchor_lang::solana_program::{self, program::invoke_signed};
-use mpl_token_metadata::instruction::{create_metadata_accounts_v3, create_master_edition_v3};
-use mpl_token_metadata::state::Creator;
-
-#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct CreatorStruct {
-    pub address: Pubkey,
-    pub share: u8,
-}
 
 #[derive(Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct CreateCollectionParams {
-    pub creator_key: Pubkey,
     pub max_supply: u64,
     pub name: String,
     pub symbol: String,
-    pub uri: String,
+    pub base_uri: String,
 }
 
 #[derive(Accounts)]
@@ -46,59 +46,45 @@ pub struct CreateCollection<'info> {
 
     #[account(
         init, 
-        seeds = [SEED_PREFIX_MINT],
+        seeds = [SEED_PREFIX_COLLECTION],
         bump,
         payer = owner,
         mint::decimals = 0,
-        mint::authority = token_mint.key(),
-        mint::freeze_authority = token_mint.key(),
+        mint::authority = collection_mint,
+        mint::freeze_authority = collection_mint,
     )]
-    pub token_mint: Account<'info, Mint>,
+    pub collection_mint: Account<'info, Mint>,
 
     /// Token Account. Its an Associated Token Account that will hold the
     /// tokens that are bridged out. It is owned by the program.
     /// Locked tokens will be transferred to this account
     #[account(
         init,
-        seeds = [SEED_PREFIX_COLLECTION, token_mint.key().as_ref()],
-        bump,
         payer = owner,
-        token::mint = token_mint,
-        token::authority = token_account_collection,
+        associated_token::mint = collection_mint,
+        associated_token::authority = owner,
     )]
-    pub token_account_collection: Account<'info, TokenAccount>,
+    pub token_account: Account<'info, TokenAccount>,
 
-    ///CHECK: Metadata Account
+    /// CHECK:
     #[account(
         mut,
-        seeds = [
-            b"metadata",
-            mpl_token_metadata::id().as_ref(),
-            token_mint.key().as_ref(),
-        ],
-        bump,
-        seeds::program = mpl_token_metadata::id()  
+        address=find_metadata_account(&collection_mint.key()).0
     )]
     pub metadata_account: UncheckedAccount<'info>,
 
-    ///CHECK: Master Edition 
+    /// CHECK:
     #[account(
-        mut, 
-        seeds = [
-            b"metadata",
-            mpl_token_metadata::id().as_ref(),
-            token_mint.key().as_ref(),
-            b"edition",
-        ],
-        bump,
-        seeds::program = mpl_token_metadata::id(),
+        mut,
+        address=find_master_edition_account(&collection_mint.key()).0
     )]
     pub master_edition: UncheckedAccount<'info>,
 
+    
     pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub metadata_program: Program<'info, Metadata>,
-  
-    pub clock: Sysvar<'info, Clock>,
+
     pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
 }
@@ -111,120 +97,99 @@ impl CreateCollection<'_> {
     ) -> Result<()> {
         let config = &mut ctx.accounts.config;
 
-        // Set the Max and Minted Supply
+        // Set the Max, Minted Supply and Base URI
         config.max_supply = params.max_supply;
         config.minted_supply = 0;
+        config.base_uri = params.base_uri.clone();
 
-        // Time to create a Collection NFT
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.token_mint.to_account_info(),
-            to: ctx.accounts.token_account_collection.to_account_info(),
-            authority: ctx.accounts.token_mint.to_account_info(),
-        };
-
+        // PDA Seed for Signing
         let bump = *ctx
             .bumps
-            .get("token_mint")
+            .get("collection_mint")
             .ok_or(ErrorFactory::BumpNotFound)?;
 
-        let cpi_signer_seeds = &[
-            b"spl_cat_nft".as_ref(),
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            SEED_PREFIX_COLLECTION,
             &[bump],
-        ];
-        let cpi_signer = &[&cpi_signer_seeds[..]];
+        ]];
 
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, cpi_signer);
-
-        mint_to(cpi_ctx, 1)?;
+        // mint collection nft
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                    authority: ctx.accounts.collection_mint.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            1,
+        )?;
 
         msg!("Token Minted !!!");
 
-        let bump = *ctx
-            .bumps
-            .get("token_mint")
-            .ok_or(ErrorFactory::BumpNotFound)?;
-
-            let signer_seeds = &[
-                b"spl_cat_nft".as_ref(),
-                &[bump],
-            ];
-
-        // Create Metadata for the token.
-        
-            
-            let creator = vec![
-                Creator {
-                    address: params.creator_key,
+        // create metadata account for collection nft
+        create_metadata_accounts_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(), // use pda mint address as mint authority
+                    update_authority: ctx.accounts.collection_mint.to_account_info(), // use pda mint as update authority
+                    payer: ctx.accounts.owner.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            DataV2 {
+                name: params.name.clone(),
+                symbol: params.symbol.clone(),
+                uri: params.base_uri.clone(),
+                seller_fee_basis_points: 0,
+                creators: Some(vec![Creator {
+                    address: ctx.accounts.owner.key(),
                     verified: false,
                     share: 100,
-                },
-            ];
-
-            let create_metadata_account_ix = create_metadata_accounts_v3(
-                ctx.accounts.metadata_program.key(),
-                ctx.accounts.metadata_account.key(),
-                ctx.accounts.token_mint.key(),
-                ctx.accounts.token_mint.key(),
-                ctx.accounts.owner.key(),
-                ctx.accounts.token_mint.key(),
-                params.name.clone(),
-                params.symbol.clone(),
-                params.uri.clone(),
-                Some(creator),
-                0,
-                true,
-                true,
-                None,
-                None,
-                None,
-            );
-
-            invoke_signed(
-                &create_metadata_account_ix,
-                &[
-                    ctx.accounts.owner.to_account_info(),
-                    ctx.accounts.metadata_account.to_account_info(),
-                    ctx.accounts.token_mint.to_account_info(),
-                    ctx.accounts.metadata_program.to_account_info(),
-                    ctx.accounts.system_program.to_account_info(),
-                ],
-                &[signer_seeds],
-            )?;
-        
-
-        // Create Master Edition
-        
-
-            let master_edition_infos = vec![
-            ctx.accounts.master_edition.to_account_info(),
-            ctx.accounts.token_mint.to_account_info(),
-            ctx.accounts.token_mint.to_account_info(),
-            ctx.accounts.owner.to_account_info(),
-            ctx.accounts.metadata_account.to_account_info(),
-            ctx.accounts.metadata_program.to_account_info(),
-            ctx.accounts.token_program.to_account_info(),
-            ctx.accounts.system_program.to_account_info(),
-            ctx.accounts.rent.to_account_info(),
-        ];
-        msg!("Master Edition Account Infos Assigned");
-        invoke_signed(
-            &create_master_edition_v3(
-                ctx.accounts.metadata_program.key(),
-                ctx.accounts.master_edition.key(),
-                ctx.accounts.token_mint.key(),
-                ctx.accounts.token_mint.key(),
-                ctx.accounts.token_mint.key(),
-                ctx.accounts.metadata_account.key(),
-                ctx.accounts.owner.key(),
-                Some(0),
-            ),
-            master_edition_infos.as_slice(),
-            &[&[b"spl_cat_nft".as_ref(), &[bump]]],
+                }]),
+                collection: None,
+                uses: None,
+            },
+            true,
+            true,
+            Some(CollectionDetails::V1 { size: params.max_supply }), // set as collection nft
         )?;
 
-        
+         // create master edition account for collection nft
+         create_master_edition_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.metadata_program.to_account_info(),
+                CreateMasterEditionV3 {
+                    payer: ctx.accounts.owner.to_account_info(),
+                    mint: ctx.accounts.collection_mint.to_account_info(),
+                    edition: ctx.accounts.master_edition.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    metadata: ctx.accounts.metadata_account.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            Some(0),
+        )?;
 
+        // verify creator on metadata account
+        sign_metadata(CpiContext::new(
+            ctx.accounts.metadata_program.to_account_info(),
+            SignMetadata {
+                creator: ctx.accounts.owner.to_account_info(),
+                metadata: ctx.accounts.metadata_account.to_account_info(),
+            },
+        ))?;
 
         Ok(())
     }
